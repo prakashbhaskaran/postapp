91ddb9a5e81c45119465c0014ab927bbc56bed1f
Prakash B <prakash.b@gumstack.com>
2022-05-19 18:00:06 +0530

    removed turbo

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,4 @@ class Post < ApplicationRecord
     belongs_to :user
     validates :title, presence: true
     validates :content, presence: true
-    after_create_commit { broadcast_prepend_to :all_posts }
-    after_destroy_commit { broadcast_remove_to :all_posts }
-    after_update_commit { broadcast_replace_to :all_posts }
 end
