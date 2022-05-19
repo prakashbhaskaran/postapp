4a5178efdf1cff49b16ec80519694512a7bce57d
Prakash B <prakash.b@gumstack.com>
2022-05-19 16:45:41 +0530

    small tweak

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
     belongs_to :user
     validates :title, presence: true
     validates :content, presence: true
-    after_create_commit { broadcast_prepend_to :posts }
-    after_destroy_commit { broadcast_remove_to :posts }
-    after_update_commit { broadcast_replace_to :posts }
+    after_create_commit { broadcast_prepend_to :all_posts }
+    after_destroy_commit { broadcast_remove_to :all_posts }
+    after_update_commit { broadcast_replace_to :all_posts }
 end
