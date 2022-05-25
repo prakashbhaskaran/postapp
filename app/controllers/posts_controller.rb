8ee597f4cc586b0aab60e0dea3649a08d85c087f
Prakash B <prakash.b@gumstack.com>
2022-05-25 07:46:34 +0530

    redirect fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -57,7 +57,7 @@ class PostsController < ApplicationController
     @current_user_post.destroy
 
     respond_to do |format|
-      format.html { redirect_to posts_url }
+      format.html { redirect_to root_path }
       format.json { head :no_content }
     end
   end
